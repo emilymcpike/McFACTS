dd105beb113ce5eea272e5972c21a961c12fd6d1
Kaila Nathaniel <37944273+spacekaila@users.noreply.github.com>
2024-11-20 14:30:16 -0500

    added function to calculate rg_in_meters

--- src/mcfacts/constants.py
+++ src/mcfacts/constants.py
@@ -7,4 +7,13 @@ Global Variables:
 
 """
 
-mass_per_msun = 1.99e30
+from astropy import constants as const
+
+# mass_per_msun = 1.99e30
+
+
+def rg_in_meters(smbh_mass):
+    smbh_mass_units = smbh_mass * const.M_sun
+    rg = (const.G * (smbh_mass_units) / (const.c ** 2.0)).to("meter")
+
+    return (rg.value)
