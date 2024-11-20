d75832b825ae3f4256522acbea6312e4352ae72b
Kaila Nathaniel <37944273+spacekaila@users.noreply.github.com>
2024-11-20 14:40:15 -0500

    there's already a function to calculate distances from rg to m

--- src/mcfacts/constants.py
+++ src/mcfacts/constants.py
@@ -10,10 +10,3 @@ Global Variables:
 from astropy import constants as const
 
 # mass_per_msun = 1.99e30
-
-
-def rg_in_meters(smbh_mass):
-    smbh_mass_units = smbh_mass * const.M_sun
-    rg = (const.G * (smbh_mass_units) / (const.c ** 2.0)).to("meter")
-
-    return (rg.value)
